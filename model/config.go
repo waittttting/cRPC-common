4f120fe56436bb088c2d00663e71bcfc236cfaa3
waittting <changjs@wdcloud.cc>
2021-04-12 17:00:22 +0800

    feat: add some config struct

--- model/config.go
+++ model/config.go
@@ -1,7 +1,37 @@
 package model
 
+/**
+ * @Description: 从云端（配置中心）获取的配置信息
+ */
+type CloudConfigInfo struct {
+	// 本服务在配置中心的配置
+	ServerConfig ServerConfig `json:"server_config"`
+	// 订阅的服务的信息
+	SubServersInfos []*SubServerInfos `json:"sub_servers_infos"`
+	// 控制中心 URL
+	ControlCenterAddr ControlCenterAddr `json:"control_center_addr"`
+}
+
 type ServerConfig struct {
+	// 服务名
 	ServerName string `json:"server_name"`
+	// 服务版本号
 	ServerVersion string `json:"server_version"`
-	Config string `json:"config"`
+	// 订阅的服务的名字的列表
+	SubServers string `json:"sub_servers"`
+}
+
+type ControlCenterAddr struct {
+	Host     string `json:"host"`
+	TcpPort  string `json:"tcp_port"`
+	HttpPort string `json:"http_port"`
+}
+
+type SubServerInfos struct {
+	ServerName string   `json:"server_name"`
+	Infos      []string `json:"infos"`
+}
+
+type PortConfig struct {
+	Port int `json:"port"`
 }
